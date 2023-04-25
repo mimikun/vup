346a0d1edde61329a20595afeee7fd23547f5884
Yuto Tanaka <karaage_nennen@icloud.com>
2023-04-25 18:15:36 +0900

    Add Invoke-Vupueue.ps1

---
+++ Invoke-Vupueue.ps1
@@ -0,0 +1,10 @@
+﻿function Invoke-Vupueue() {
+    # Update chocolatey packages
+    gsudo choco upgrade all
+
+    # Update git for windows
+    git update-git-for-windows
+
+    # Update cargo packages
+    Invoke-PueueUpdateCargoPackage
+}
