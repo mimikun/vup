75e6a0ca77b0f34fa5883474d5a4cb290568ab1a
Yuto Tanaka <karaage_nennen@icloud.com>
2023-10-02 22:42:51 +0900

    Update Invoke-Vup.ps1

--- Invoke-Vup.ps1
+++ Invoke-Vup.ps1
@@ -1,11 +1,18 @@
 ﻿function Invoke-Vup() {
     # Update scoop
-    scoop update
     scoop update -a
+
+    # Update rustup
+    rustup update
 
     # Update git for windows
     git update-git-for-windows
 
     # Update cargo packages
     Invoke-UpdateCargoPackage
+
+    # Run Lazy update
+    Write-Output "Type ``vim`` and Type ``u``"
 }
+
+Set-Alias -Name vup -Value Invoke-Vup
