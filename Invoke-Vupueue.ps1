d92276896526ebbb2a248257ea8cd63c1b226c14
Yuto Tanaka <karaage_nennen@icloud.com>
2023-10-02 22:43:01 +0900

    Update Invoke-Vupueue.ps1

--- Invoke-Vupueue.ps1
+++ Invoke-Vupueue.ps1
@@ -1,11 +1,18 @@
 ﻿function Invoke-Vupueue() {
     # Update scoop
-    scoop update
     scoop update -a
 
     # Update git for windows
     git update-git-for-windows
 
+    # Update rustup
+    rustup update
+
     # Update cargo packages
     Invoke-PueueUpdateCargoPackage
+
+    # Run Lazy update
+    Write-Output "Type ``vim`` and Type ``u``"
 }
+
+Set-Alias -Name vupueue -Value Invoke-Vupueue
